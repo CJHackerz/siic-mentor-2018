42355a72907a2548b9d2d3b9a71a8d46475fa4dc
Rohan Anand <thededlier@gmail.com>
2018-01-28 23:48:51 +0530

    signup template ready

--- auth/signup.php
+++ auth/signup.php
@@ -108,6 +108,70 @@
 
           <h4 class="mb-3">Professional Details</h4>
 
+          <div class="mb-3">
+            <label for="aot">Area of Interest</label>
+            <input type="text" class="form-control" id="aot" name="mentor_aot" required>
+            <div class="invalid-feedback">
+              Please enter your Area of Interest
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-5 mb-3">
+              <label for="startup">Do you have any experience in mentoring startups or woking with startups</label>
+              <select class="custom-select d-block w-100" id="startup" name="mentor_startup" required>
+                <option value="">Choose...</option>
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </select>
+              <div class="invalid-feedback">
+                Please select a valid option
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-5 mb-3">
+              <label for="startup">What kind of value addition can you make to a startup team?</label>
+              <select class="custom-select d-block w-100" id="startup" name="mentor_val_add" required>
+                <option value="">Choose...</option>
+                <option value="Idea to Prototype(Technical)">Idea to Prototype(Technical)</option>
+                <option value="Product to Market(Business)">Product to Market(Business)</option>
+                <option value="Networking">Networking</option>
+                <option value="Source of Funds/Investors">Source of Funds/Investors</option>
+                <option value="Financial Management">Financial Management</option>
+                <option value="Legal Assistance">Legal Assistance</option>
+                <option value="Organization Development">Organization Development</option>
+                <option value="Others">Others</option>
+              </select>
+              <div class="invalid-feedback">
+                Please select a valid option
+              </div>
+            </div>
+          </div>
+
+          <div class="mb-3">
+            <label for="startup">What will be your comfortable hours of mentoring per week?</label>
+            <input type="text" class="form-control" id="startup" name="mentor_time" required>
+            <div class="invalid-feedback">
+              Please enter your preferred timing
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-5 mb-3">
+              <label for="startup">Are you interested to take special training to mentor startups?</label>
+              <select class="custom-select d-block w-100" id="startup" name="mentor_spec_train" required>
+                <option value="">Choose...</option>
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </select>
+              <div class="invalid-feedback">
+                Please select a valid option
+              </div>
+            </div>
+          </div>
+
           <hr class="mb-4">
           <button class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
         </form>
