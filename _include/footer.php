595f72e3295cbdfee217067334784bc8aaa0d862
Rohan Anand <thededlier@gmail.com>
2018-01-31 00:28:24 +0530

    fixed footer

--- _include/footer.php
+++ _include/footer.php
@@ -1,10 +1,8 @@
-<head>
+  </main>
   <style>
   .cosmos {
-    position:absolute;
-    bottom:0;
     width:100%;
-
+    margin-top: 20px;
     background-color: #797572;
     background-image: -webkit-linear-gradient(263deg, rgba(255, 255, 255, 0.2) 0%, #000000 100%), -webkit-linear-gradient(18deg, rgba(255, 255, 255, 0.25) 25%, rgba(0, 0, 0, 0.25) 100%), -webkit-radial-gradient(47% 102%, rgba(255, 255, 255, 0.5) 0%, rgba(21, 24, 32, 0.6) 120%);
     background-image: linear-gradient(-173deg, rgba(255, 255, 255, 0.2) 0%, #000000 100%), linear-gradient(72deg, rgba(255, 255, 255, 0.25) 25%, rgba(0, 0, 0, 0.25) 100%), radial-gradient(47% 102%, rgba(255, 255, 255, 0.5) 0%, rgba(21, 24, 32, 0.6) 120%);
@@ -12,18 +10,19 @@
     padding: 20px 0 20px 0;
   }
   </style>
-</head>
-<footer class="cosmos">
-  <div class="container" style="width:100%; ">
-    <div class="row">
-      <div class="col-md-8 hidden-xs-down">
-        Site in Alpha
-      </div>
-      <div class="col-md-4 col-xs-12">
-        <a href="https://webarchsrm.com/">
-          <img alt="webarch logo" class="pull-right" src="../assets/weblogo.png" />
-        </a>
+  <footer class="cosmos">
+    <div class="container" style="width:100%; ">
+      <div class="row">
+        <div class="col-md-8 hidden-xs-down">
+          Site in Alpha
+        </div>
+        <div class="col-md-4 col-xs-12">
+          <a href="https://webarchsrm.com/">
+            <img alt="webarch logo" class="pull-right" src="../assets/weblogo.png" />
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-</footer>
+  </footer>
+</body>
+</html>
